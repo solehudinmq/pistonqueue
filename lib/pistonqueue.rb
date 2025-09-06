df909bcc1a47385bf542ca9f082e35075a132d5f
SolehMQ <solehudinmq@gmail.com>
2025-09-06 13:42:43 +0700

    first commit

---
+++ lib/pistonqueue.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "pistonqueue/version"
+
+module Pistonqueue
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/pistonqueue_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Pistonqueue do
+  it "has a version number" do
+    expect(Pistonqueue::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
